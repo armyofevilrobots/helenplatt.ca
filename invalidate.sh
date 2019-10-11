94b34f0538b931235e00ec8deae1e1f0559333dc
Derek Anderson <derek@armyofevilrobots.com>
2019-10-11 12:25:49 -0700

    Fix invalidation command.

--- invalidate.sh
+++ invalidate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-aws cloudfront create-invalidation \
-    --paths '/*' \
-    --distribution-id $AWS_CLOUDFRONT_DISTRIBUTION_ID
+set -e
+echo "Invalidation of distribution cache"
+aws cloudfront create-invalidation --paths '/*' --distribution-id $1 2>&1
+echo "Done invalidation"
